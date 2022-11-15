5f7bd38ab42b5174d7183d10aace4650e6a53b8b
YerayJ <you@example.com>
2022-11-15 18:17:20 +0100

    Ejercicio3 terminado

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -4,34 +4,42 @@ import java.util.Scanner;
 
 /*
  * PRUEBAS
- * 0 veces : num = 1 / res-esperado = Hay un total de 0 números primos. / res = Hay un total de 0 números primos.
- * 1 vez : num = 2 / res-esperado = Hay un total de 0 números primos. / res = Hay un total de 1 números primos.
- * Varias veces : num = 9 / res-esperado = Hay un total de 4 números primos. / Hay un total de 4 números primos.
+ * 0 veces : num = 0 / res-esperado = . / res = .
+ * 1 vez : num = 1 / res-esperado = " *". / res = " *".
+ * Varias veces : num = 9 /
+ * res-esperado =  *
+ *                * *
+ *   res =  *
+ *         * *
  */
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        // Creamos las variables num para pedirsela al usuario y un contador.
+        // Creamos la variable size para pedírsela al usuario.
         int size;
 
         // Creamos escaner
         Scanner key = new Scanner(System.in);
 
-        // Pedimos las variables al usuario
+        // Pedimos la variable al usuario
         System.out.println("Deme un tamaño: ");
         size = key.nextInt();
 
         // Creamos el primer bucle que recorre el rango entre la i y el número pasado
         // por pantalla.
         for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size-i ; j++) {
-                System.out.println(" ");
-            } 
-            for (int j = 0; j < args.length; j++) {
-                
+            // En el siguiente bucle tendremos que ir contando los espacios por cada línea
+            // desde el 0 hasta un número antes del tamaño total.
+            for (int espacio = 0; espacio < size - i; espacio++) {
+                // Aquí imprimimos los espacios sin el salto de línea.
+                System.out.print(" ");
+            } // El for en este caso va a ir añadiendo asteriscos por línea hasta la i que sería la linea. 
+            for (int j = 0; j < i; j++) {
+                // Aquí imprimimos los asteriscos sin el salto de línea.
+                System.out.print("* ");
             }
-            
-            
+            // Por último imprimimos saltos de línea 
+            System.out.println();
         }
 
         // Cerramos el escaner
