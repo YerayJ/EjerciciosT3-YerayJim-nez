e1dd4cb12771c5bd2eac7fccc58d1b080e376dbf
YerayJ <yjimenez@iesnervion.es>
2022-11-16 14:42:11 +0100

    Ejercicioas

--- src/ejercicios/Ejercicio6.java
+++ src/ejercicios/Ejercicio6.java
@@ -4,21 +4,15 @@ import java.util.Scanner;
 
 /*
  * PRUEBAS
- * 0 veces : num= -1 / res-esperado = . / res = . 
- * 1 vez : num= 1 / res-esperado = 1 / res = 1 
+ * 0 veces : num1 = 0 num2 = 0 / res-esperado = No se puede dividir entre 0. / res = No se puede dividir entre 0. 
+ * 1 vez : num1 = 1 num2 = 1 / res-esperado = El mínimo común múltiplo es 1 / res = El mínimo común múltiplo es 1
  * Varias veces : num = 3 / 
- * res-esperado = 1
- *                22
- *                333
- * res          = 1
- *                22
- *                333
  */
 
 public class Ejercicio6 {
     public static void main(String[] args) {
         // Creamos la variable num para pedírsela al usuario.
-        int num1,num2,mcm;
+        int num1, num2, mcm;
         int mcd = 0;
         // Creamos escaner
         Scanner key = new Scanner(System.in);
@@ -31,12 +25,15 @@ public class Ejercicio6 {
         // Creamos un for
         for (int i = 1; i <= num1 && i <= num2; i++) {
             if (num1 % i == 0 && num2 % i == 0) {
-                mcd=i;
- 
-            } 
+                mcd = i;
+            }
         }
-        mcm = (num1*num2/mcd);
-        System.out.println("El mínimo común múltiplo es " + mcm);  
+        if (num1 != 0 && num2 != 0) {
+            mcm = (num1 * num2 / mcd);
+            System.out.println("El mínimo común múltiplo es " + mcm);
+        } else {
+            System.out.println("No se puede dividir entre 0");
+        }
         // Cerramos el escaner
         key.close();
     }
