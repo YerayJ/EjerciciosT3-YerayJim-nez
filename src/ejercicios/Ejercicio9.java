69bf0d67c3fc623916bb70f2a400265677e6fb7c
YerayJ <yjimenez@iesnervion.es>
2022-11-17 14:11:06 +0100

    Ejercicio9 terminado

--- src/ejercicios/Ejercicio9.java
+++ src/ejercicios/Ejercicio9.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 
 /*
  * PRUEBAS
- * 0 veces : num= 0 / res-esperado = Total de número introducidos 0 Número de fallos 0. / res = Total de número introducidos 0 Número de fallos 0. 
- * 1 vez : num= 15 num = 0 / res-esperado = Total de número introducidos 1 Número de fallos 1 / res = Total de número introducidos 1 Número de fallos 1 
- * Varias veces : num = 99 num = 14 num= 80 num = 0 / Total de número introducidos 3 Número de fallos 1 / Total de número introducidos 3 Número de fallos 1 
+ * 0 veces : num= 0 / res-esperado = El número es menor que 0 / res = El número es menor que 0
+ * 1 vez : num= 1 / res-esperado = El número tiene: 1 cifras / res = El número tiene: 1 cifras
+ * Varias veces : num = 3400 / res-esperado = El número tiene: 4 cifras / res = El número tiene: 4 cifras
  */
 
 public class Ejercicio9 {
     public static void main(String[] args) {
         // Creamos la variable num para pedírsela al usuario.
-        int num, cifra;
-
+        int num;
+        // Esta variable será la auxiliar.
+        int decimal = 1;
+        // Esta variable la usaremos como contador.
+        int cifras = 0;
 
         // Creamos escaner
         Scanner key = new Scanner(System.in);
@@ -21,26 +24,23 @@ public class Ejercicio9 {
         // Pedimos las variable al usuario
         System.out.println("Deme un número inicial: ");
         num = key.nextInt();
-
-        // Creamos un bucle en el que se va ejecutando mientras num sea mayor que 0.
-        for (int i = 0; num > 0; i++) {
-            // Por cada vuelta al bucle le añadimos uno al contador.
-            contador++;
-            // Crearemos un número aletorio por cada vuelta de rango del 1 al 100
-            dado = (int) (Math.random() * 99 + 1);
-            // En este if contaremos los fallos, para ello num debe ser menor que el dado.
-            if (num < dado) {
-                // Acumulamos uno en la variable fallo.
-                fallo++;
-                // Imprimimos que el usuario ha fallado.
-                System.out.println("Fallo es menor");
-            } // Volvemos a pedir el número al usuario en cada vuelta.
-            System.out.print("Deme un número: ");
-            num = key.nextInt();
-        } // Por último mostramos el número total de número introducidos y fallos.
-        System.out.println("Total de número introducidos " + contador);
-        System.out.println("Número de fallos " + fallo);
-
+        // Creamos un if en el que se va ejecutando mientras num sea mayor que 0.
+        if (num > 0) {
+            // En este bucle le damos a la i el valor de num y este lo vamos diviendo en
+            // cada pasada por la variable decimal. Se ejecutará mientras sea mayor igual a
+            // 1, ya que si llegará a dividirse por 0 daría error.
+            for (int i = num; i >= 1; i = num / decimal) {
+                // Le añadimos uno a cifras por cada vuelta
+                cifras++;
+                // En la variable decimal le vamos multiplicando por 10 para ir moviendonos de
+                // posición en el número.
+                decimal *= 10;
+            }
+            // Imprimimos el resultado.
+            System.out.println("El número tiene: " + cifras + " cifras");
+        } else { // Imprimimos error en caso de ser menor-igual que 0
+            System.out.println("El número es menor que 0");
+        }
         // Cerramos el escaner
         key.close();
     }
